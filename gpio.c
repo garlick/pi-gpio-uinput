2ee33768b297dfb61bf1fce093bcf1ee6bd5126c
Jim Garlick <garlick.jim@gmail.com>
2013-05-25 16:30:59 -0700

    Add license headers.

--- gpio.c
+++ gpio.c
@@ -1,3 +1,25 @@
+/*****************************************************************************
+ *  Copyright (C) 2013 Jim Garlick
+ *  Written by Jim Garlick <garlick.jim@gmail.com>
+ *  All Rights Reserved.
+ *
+ *  This file is part of pi-gpio-uinput.
+ *  For details, see <https://github.com/garlick/pi-gpio-uinput>
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License (as published by the
+ *  Free Software Foundation) version 2, dated June 1991.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE. See the terms and conditions of the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software Foundation,
+ *  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA or see
+ *  <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
 /* gpio.c - return a stream of keypress events from pi GPIO */
 
 /* Inputs are configured to interrupt on rising and falling edges.
